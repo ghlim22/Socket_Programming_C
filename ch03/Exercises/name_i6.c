e15bebb2331366b72c6e40ec4b08e3db841f08e3
Ubuntu <lim@vm01.43nbdgytbe4exlcexfk12ri4ob.syx.internal.cloudapp.net>
2024-05-11 12:32:49 +0000

    name_i6

--- ch03/Exercises/name_i6.c
+++ ch03/Exercises/name_i6.c
@@ -15,14 +15,22 @@ int main(int argc, char *argv[])
     memset(&addr, 0, sizeof(addr));
     addr.sin6_family = AF_INET;
 	addr.sin6_port = htons(atoi(argv[2]));
-    int ret = inet_pton(AF_INET6, argv[1], &addr.sin6_addr);
+    int ret = inet_pton(AF_INET6, argv[1], &addr.sin6_addr.s6_addr);
     if (ret <= 0) {
         return 0;
     }
 
+    char buf[INET6_ADDRSTRLEN];
+    if (inet_ntop(AF_INET6, &addr.sin6_addr.s6_addr, buf, sizeof(buf)) != NULL) {
+        printf("%s/%d\n", buf, ntohs(addr.sin6_port));
+    } else {
+        ;
+    }
+
+
     int ecode =
         getnameinfo((struct sockaddr *)&addr, sizeof(addr), hostname_buf,
-                    sizeof(hostname_buf), service_buf, sizeof(service_buf), 0);
+                    sizeof(hostname_buf), service_buf, sizeof(service_buf), NI_NUMERICHOST);
 
 	if (ecode != 0) {
 		return 0;
